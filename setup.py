4ba42b9959f9deb426b7bec81ed80111b6db7056
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-03-03 10:22:42 +0800

    Update install info

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='locon',
     packages=['locon'],
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/KohakuBlueleaf/LoCon',
     description='LoRA for Convolution Network',
     author='Shih-Ying Yeh(KohakuBlueLeaf)',
