f1fbc485395f5e3fb9905bcd2f64a0f30681af6a
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-07-01 15:45:19 +0800

    add __init__.py for modules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='lycoris_lora',
     packages=['lycoris'],
-    version='1.8.0.dev2',
+    version='1.8.0.dev3',
     url='https://github.com/KohakuBlueleaf/LyCORIS',
     description='Lora beYond Conventional methods, Other Rank adaptation Implementations for Stable diffusion',
     author='Shih-Ying Yeh(KohakuBlueLeaf), Yu-Guan Hsieh, Zhidong Gao',
