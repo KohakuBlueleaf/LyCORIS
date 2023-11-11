151a7304dfb69d466ade88c9d40d84d66a877539
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-11-12 00:11:05 +0800

    merge support sdxl

--- tools/merge.py
+++ tools/merge.py
@@ -22,6 +22,10 @@ def get_args():
         default=False, action="store_true"
     )
     parser.add_argument(
+        "--is_sdxl", help="Your base/db model is sdxl or not",
+        default=False, action="store_true"
+    )
+    parser.add_argument(
         "--device", help="Which device you want to use to merge the weight",
         default='cpu', type=str
     )
@@ -34,7 +38,7 @@ def get_args():
         default='1.0', type=float
     )
     return parser.parse_args()
-ARGS = get_args()
+args = ARGS = get_args()
 
 
 from lycoris.utils import merge
@@ -43,13 +47,20 @@ from lycoris.kohya.model_utils import (
     save_stable_diffusion_checkpoint,
     load_file
 )
+from lycoris.kohya.sdxl_model_util import (
+    load_models_from_sdxl_checkpoint,
+    save_stable_diffusion_checkpoint as save_sdxl_checkpoint
+)
 
 import torch
 
 
 @torch.no_grad()
 def main():
-    base = load_models_from_stable_diffusion_checkpoint(ARGS.is_v2, ARGS.base_model)
+    if args.is_sdxl:
+        base = load_models_from_sdxl_checkpoint(None, args.base_model)
+    else:
+        base = load_models_from_stable_diffusion_checkpoint(args.is_v2, args.base_model)
     if ARGS.lycoris_model.rsplit('.', 1)[-1] == 'safetensors':
         lyco = load_file(ARGS.lycoris_model)
     else:
@@ -67,19 +78,35 @@ def main():
     if dtype is None:
         raise ValueError(f'Cannot Find the dtype "{dtype}"')
     
+    if args.is_sdxl:
+        base_tes = [base[0], base[1]]
+        base_unet = base[3]
+    else:
+        base_tes = [base[0]]
+        base_unet = base[2]
+    
     merge(
-        base,
+        base_tes,
+        base_unet,
         lyco,
         ARGS.weight,
         ARGS.device
     )
     
-    save_stable_diffusion_checkpoint(
-        ARGS.is_v2, ARGS.output_name, 
-        base[0].cpu(), base[2].cpu(), 
-        None, 0, 0, dtype, 
-        base[1]
-    )
+    if args.is_sdxl:
+        save_sdxl_checkpoint(
+            ARGS.output_name, 
+            base[0].cpu(), base[1].cpu(), base[3].cpu(),
+            None, 0, 0, dtype, 
+            base[2]
+        )
+    else:
+        save_stable_diffusion_checkpoint(
+            ARGS.is_v2, ARGS.output_name, 
+            base[0].cpu(), base[2].cpu(), 
+            None, 0, 0, dtype, 
+            base[1]
+        )
 
 
 if __name__ == '__main__':
