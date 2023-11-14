903c23e38ae4a65f9b6d0b1e43bd6de06774916b
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-11-15 01:11:20 +0800

    Update flops_calc.py

--- experiments/flops_calc.py
+++ experiments/flops_calc.py
@@ -26,19 +26,19 @@ def mac_for_conv_bax(in_h, in_w, in_dim, out_dim, rank):
 
 
 def mac_for_lin_wba(in_dim, out_dim, rank):
-    return mac_matmul(in_dim, rank, out_dim) + in_dim * out_dim
+    return mac_matmul(in_dim, rank, out_dim)
 
 
 def mac_for_conv_wba(in_dim, out_dim, rank):
-    return mac_matmul(in_dim, rank, out_dim * 3**2) + in_dim * out_dim * 3**2
+    return mac_matmul(in_dim, rank, out_dim * 3**2)
 
 
 # shape list for 64x64 input of SD Unet
 shape_list = [
-    (320, 64, 64),
-    (640, 32, 32),
-    (1280, 16, 16),
-    (1280, 8, 8)
+    (320, 96, 96),
+    (640, 48, 48),
+    (1280, 24, 24),
+    (1280, 12, 12)
 ]
 
 mac_lin_bax = 0
@@ -54,16 +54,31 @@ for shape in shape_list:
     original_conv = conv_layer_mac(shape[1], shape[2], shape[0], shape[0], 3, 1, 1)
     
     mac_lin_bax = mac_for_lin_bax(shape[1]*shape[2], shape[0], shape[0], rank)
-    mac_conv_bax = mac_for_conv_bax(shape[1], shape[2], shape[0], shape[0], rank)
+    mac_lin_backward_bax = (
+        mac_matmul(rank, shape[1]*shape[2], shape[0])   # dB = dY*(AX)
+        + mac_matmul(shape[1]*shape[2], shape[0], rank) # d(AX) = B*dY
+        + mac_matmul(rank, shape[1]*shape[2], shape[0]) # dA = d(AX)*X
+        + mac_matmul(shape[1]*shape[2], rank, shape[0]) # dX = A*d(AX)
+    )
     mac_lin_wba = mac_for_lin_wba(shape[0], shape[0], rank)
+    mac_lin_backward_wba = mac_matmul(rank, shape[0], shape[0])*2
+    
+    mac_conv_bax = mac_for_conv_bax(shape[1], shape[2], shape[0], shape[0], rank)
+    mac_conv_backward_bax = (
+        
+    )# 2 times of forward I guess
     mac_conv_wba = mac_for_conv_wba(shape[0], shape[0], rank)
+    mac_conv_backward_wba = mac_matmul(rank, shape[0]*3**2, shape[0]) + mac_matmul(rank, shape[0], shape[0]*3**2)
 
-    print(f"lin w size   : {shape[0] * shape[0]}")
-    print(f"original_lin : {original_lin}")
-    print(f"mac_lin_bax  : {mac_lin_bax}")
-    print(f"mac_lin_wba  : {mac_lin_wba}")
-    print(f"conv w size  : {shape[0] * shape[0] * 3**2}")
-    print(f"original_conv: {original_conv}")
-    print(f"mac_conv_bax : {mac_conv_bax}")
-    print(f"mac_conv_wba : {mac_conv_wba}")
+    print(f"lin")
+    print(f"original_lin  : {original_lin}")
+    print(f"mac_lin_bax   : {mac_lin_bax}")
+    print(f"mac_lin_bwax  : {mac_lin_backward_bax}")
+    print(f"mac_lin_wba   : {mac_lin_wba}")
+    print(f"mac_lin_bwba  : {mac_lin_backward_wba}")
+    print(f"conv")
+    print(f"original_conv : {original_conv}")
+    print(f"mac_conv_bax  : {mac_conv_bax}")
+    print(f"mac_conv_wba  : {mac_conv_wba}")
+    print(f"mac_conv_bwba : {mac_conv_backward_wba}")
     print()
