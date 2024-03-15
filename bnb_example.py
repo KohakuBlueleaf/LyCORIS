e253c3cb0c862b5d66b9c8d3b4414b13fb780cfb
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-03-15 16:46:08 +0800

    Formatting

--- bnb_example.py
+++ bnb_example.py
@@ -4,16 +4,9 @@ from lycoris import create_lycoris, LycorisNetwork
 model = AutoModelForCausalLM.from_pretrained("KBlueLeaf/DanTagGen", load_in_4bit=True)
 
 
-LycorisNetwork.apply_preset(
-    {"target_name": [".*proj.*"]}
-)
+LycorisNetwork.apply_preset({"target_name": [".*proj.*"]})
 lycoris_net = create_lycoris(
-    model, 
-    1.0, 
-    linear_dim=16, 
-    linear_alpha=2.0, 
-    algo="lokr",
-    factor=8
+    model, 1.0, linear_dim=16, linear_alpha=2.0, algo="lokr", factor=8
 )
 lycoris_net.apply_to()
 
