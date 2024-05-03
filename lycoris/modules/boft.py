6d6f356a034511d08e9adc1c87ada1fe03719c5e
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-05-03 20:46:18 +0800

    BOFT new base

--- lycoris/modules/boft.py
+++ lycoris/modules/boft.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 from einops import rearrange
 
-from .base import ModuleCustomSD
+from .base import ModuleCustomSD, LycorisBaseModule
 from ..logging import logger
 from ..utils.bnb import LinearNF4, QuantLinears, log_bypass
 
@@ -48,10 +48,13 @@ def butterfly_factor(dimension: int, factor: int = -1) -> tuple[int, int]:
     return dimension // n, n
 
 
-class ButterflyOFTModule(ModuleCustomSD):
-    """
-    modifed from kohya-ss/sd-scripts/networks/lora:LoRAModule
-    """
+class ButterflyOFTModule(LycorisBaseModule):
+    support_module = {
+        "linear",
+        "conv1d",
+        "conv2d",
+        "conv3d",
+    }
 
     def __init__(
         self,
@@ -71,31 +74,18 @@ class ButterflyOFTModule(ModuleCustomSD):
         bypass_mode=False,
         **kwargs,
     ):
-        """if alpha == 0 or None, alpha is rank (no scaling)."""
-        super().__init__()
-        self.lora_name = lora_name
-
-        if isinstance(org_module, nn.Linear):
-            self.op = F.linear
-            self.dim = org_module.out_features
-            self.kw_dict = {}
-        elif isinstance(org_module, nn.Conv2d):
-            self.op = F.conv2d
-            self.dim = org_module.out_channels
-            self.kw_dict = {
-                "stride": org_module.stride,
-                "padding": org_module.padding,
-                "dilation": org_module.dilation,
-                "groups": org_module.groups,
-            }
-        else:
-            raise NotImplementedError
-
-        if isinstance(org_module, QuantLinears):
-            if not bypass_mode:
-                log_bypass()
-            bypass_mode = True
-        self.bypass_mode = bypass_mode
+        super().__init__(
+            lora_name,
+            org_module,
+            multiplier,
+            dropout,
+            rank_dropout,
+            module_dropout,
+            rank_dropout_scale,
+            bypass_mode,
+        )
+        if self.module_type not in self.support_module:
+            raise ValueError(f"{self.module_type} is not supported in BOFT algo.")
 
         out_dim = self.dim
         b, m_exp = butterfly_factor(out_dim, lora_dim)
@@ -116,28 +106,9 @@ class ButterflyOFTModule(ModuleCustomSD):
                 torch.ones(out_dim, *(1 for _ in range(org_module.weight.dim() - 1)))
             )
 
-        self.rank_dropout = rank_dropout
-        self.rank_dropout_scale = rank_dropout_scale
-        self.module_dropout = module_dropout
-
-        self.multiplier = multiplier
-        self.org_module = [org_module]
-        self.org_forward = self.org_module[0].forward
-
     @property
     def I(self):
         return torch.eye(self.block_size, device=next(self.parameters()).device)
-
-    def apply_to(self, **kwargs):
-        self.org_forward = self.org_module[0].forward
-        self.org_module[0].forward = self.forward
-
-    def restore(self):
-        self.org_module[0].forward = self.org_forward
-
-    def merge_to(self, multiplier=1.0):
-        weight = self.make_weight(scale=multiplier)
-        self.org_module[0].weight.data.copy_(weight)
 
     def get_r(self):
         I = self.I
@@ -158,7 +129,7 @@ class ButterflyOFTModule(ModuleCustomSD):
         b = self.boft_b
         r_b = b // 2
         r = self.get_r()
-        inp = self.org_module[0].weight.to(device, dtype=r.dtype)
+        inp = self.org_weight.to(device, dtype=r.dtype)
 
         for i in range(m):
             bi = r[i]  # b_num, b_size, b_size
@@ -174,6 +145,12 @@ class ButterflyOFTModule(ModuleCustomSD):
             inp = rearrange(inp, "(c k g) ... -> (c g k) ...", g=2, k=2**i * r_b)
 
         return inp
+
+    def get_merged_weight(self, multiplier=1, shape=None, device=None):
+        diff = self.make_weight(scale=multiplier, device=device)
+        if shape is not None:
+            diff = diff.view(shape)
+        return diff, None
 
     @torch.no_grad()
     def apply_max_norm(self, max_norm, device=None):
@@ -210,7 +187,7 @@ class ButterflyOFTModule(ModuleCustomSD):
 
         return inp
 
-    def forward(self, x):
+    def forward(self, x, *args, **kwargs):
         if self.module_dropout and self.training:
             if torch.rand(1) < self.module_dropout:
                 return self.org_forward(x)
