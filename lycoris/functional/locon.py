56f0184df9b5560b031d14424456fafc9f995c99
KohakuBlueleaf <apolloyeh0123@gmail.com>
2024-05-08 10:34:21 +0200

    Start making functional api

---
+++ lycoris/functional/locon.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def lora_diff_weight(a, b, alpha=None):
+    """### lora_diff_weight
+
+    Get ΔW = BA, where BA is low rank decomposition
+
+    Args:
+        a (torch.Tensor): weight of down proj linear/conv layer
+        b (torch.Tensor): weight of up proj linear/conv layer
+        alpha (float, optional): scale factor, alpha/rank is gamma (check our paper)
+
+    Returns:
+        torch.Tensor: ΔW
+    """
+    assert a.size(0) == b.size(1)
+    R, I, *k = a.shape
+    O, R, *_ = b.shape
+    if alpha is None:
+        gamma = 1
+    else:
+        gamma = alpha / R
+
+    result = b.reshape(-1, b.size(1)) @ (a.reshape(a.size(0), -1) * gamma)
+    return result.reshape(O, I, *k)
+
+
+FUNC_LIST = [None, None, F.linear, F.conv1d, F.conv2d, F.conv3d]
+
+
+def lora_bypass_forward_diff(x, a, b, alpha=None, extra_args={}):
+    """### lora_bypass_forward_diff
+
+    Args:
+        x (torch.Tensor): input tensor
+        a (torch.Tensor): weight of down proj linear/conv layer
+        b (torch.Tensor): weight of up proj linear/conv layer
+        alpha (float, optional): scale factor, alpha/rank is gamma (check our paper)
+
+    Returns:
+        torch.Tensor: output tensor
+    """
+    assert a.size(0) == b.size(1)
+    R, I, *k = a.shape
+    O, R, *_ = b.shape
+    if alpha is None:
+        gamma = 1
+    else:
+        gamma = alpha / R
+
+    down = FUNC_LIST[a.dim()](x, a, **extra_args)
+    up = FUNC_LIST[a.dim()](down, b)
+    return up * gamma
+
+
+if __name__ == "__main__":
+    w = torch.randn(128, 128, 3, 3)
+    a = torch.randn(16, 128, 3, 3) * 0.01
+    b = torch.randn(128, 16, 1, 1) * 0.1
+    extra_args = {"padding": 1}
+
+    x = torch.randn(1, 128, 8, 8)
+    y = FUNC_LIST[a.dim()](x, w, **extra_args)
+    diff_w = lora_diff_weight(a, b, 4)
+    diff_y = lora_bypass_forward_diff(x, a, b, 4, extra_args)
+
+    print(F.mse_loss(y, y + diff_y))
+    print(F.mse_loss(w, w + diff_w))
