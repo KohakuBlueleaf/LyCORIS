0b3d92600ad785511c2fd2c051335dce2b3b8de3
KohakuBlueleaf <apolloyeh0123@gmail.com>
2024-05-09 11:08:29 +0200

    Refine the tucker decomposition for loha

--- lycoris/functional/loha.py
+++ lycoris/functional/loha.py
@@ -31,8 +31,8 @@ class HadaWeightTucker(torch.autograd.Function):
     def forward(ctx, t1, w1d, w1u, t2, w2d, w2u, scale=torch.tensor(1)):
         ctx.save_for_backward(t1, w1d, w1u, t2, w2d, w2u, scale)
 
-        rebuild1 = torch.einsum("i j k l, j r, i p -> p r k l", t1, w1d, w1u)
-        rebuild2 = torch.einsum("i j k l, j r, i p -> p r k l", t2, w2d, w2u)
+        rebuild1 = torch.einsum("i j ..., j r, i p -> p r ...", t1, w1d, w1u)
+        rebuild2 = torch.einsum("i j ..., j r, i p -> p r ...", t2, w2d, w2u)
 
         return rebuild1 * rebuild2 * scale
 
@@ -41,32 +41,32 @@ class HadaWeightTucker(torch.autograd.Function):
         (t1, w1d, w1u, t2, w2d, w2u, scale) = ctx.saved_tensors
         grad_out = grad_out * scale
 
-        temp = torch.einsum("i j k l, j r -> i r k l", t2, w2d)
-        rebuild = torch.einsum("i j k l, i r -> r j k l", temp, w2u)
+        temp = torch.einsum("i j ..., j r -> i r ...", t2, w2d)
+        rebuild = torch.einsum("i j ..., i r -> r j ...", temp, w2u)
 
         grad_w = rebuild * grad_out
         del rebuild
 
-        grad_w1d = torch.einsum("r j k l, i j k l -> r i", temp, grad_w)
-        grad_temp = torch.einsum("i j k l, i r -> r j k l", grad_w, w1u.T)
+        grad_w1d = torch.einsum("r j ..., i j ... -> r i", temp, grad_w)
+        grad_temp = torch.einsum("i j ..., i r -> r j ...", grad_w, w1u.T)
         del grad_w, temp
 
-        grad_w1u = torch.einsum("i r k l, i j k l -> r j", t1, grad_temp)
-        grad_t1 = torch.einsum("i j k l, j r -> i r k l", grad_temp, w1d.T)
+        grad_w1u = torch.einsum("i r ..., i j ... -> r j", t1, grad_temp)
+        grad_t1 = torch.einsum("i j ..., j r -> i r ...", grad_temp, w1d.T)
         del grad_temp
 
-        temp = torch.einsum("i j k l, j r -> i r k l", t1, w1d)
-        rebuild = torch.einsum("i j k l, i r -> r j k l", temp, w1u)
+        temp = torch.einsum("i j ..., j r -> i r ...", t1, w1d)
+        rebuild = torch.einsum("i j ..., i r -> r j ...", temp, w1u)
 
         grad_w = rebuild * grad_out
         del rebuild
 
-        grad_w2d = torch.einsum("r j k l, i j k l -> r i", temp, grad_w)
-        grad_temp = torch.einsum("i j k l, i r -> r j k l", grad_w, w2u.T)
+        grad_w2d = torch.einsum("r j ..., i j ... -> r i", temp, grad_w)
+        grad_temp = torch.einsum("i j ..., i r -> r j ...", grad_w, w2u.T)
         del grad_w, temp
 
-        grad_w2u = torch.einsum("i r k l, i j k l -> r j", t2, grad_temp)
-        grad_t2 = torch.einsum("i j k l, j r -> i r k l", grad_temp, w2d.T)
+        grad_w2u = torch.einsum("i r ..., i j ... -> r j", t2, grad_temp)
+        grad_t2 = torch.einsum("i j ..., j r -> i r ...", grad_temp, w2d.T)
         del grad_temp
         return grad_t1, grad_w1d, grad_w1u, grad_t2, grad_w2d, grad_w2u, None
 
