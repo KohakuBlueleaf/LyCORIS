5e0cb840b25f75497ae06346ab887f38657bdbad
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-05-17 22:22:41 +0800

    Fulfill the __init__ import of functional module

--- lycoris/functional/__init__.py
+++ lycoris/functional/__init__.py
@@ -1,4 +1,13 @@
+from .general import (
+    rebuild_tucker,
+    factorization,
+    power2factorization,
+)
 from .locon import (
     lora_diff_weight,
     lora_bypass_forward_diff,
+)
+from .loha import (
+    loha_diff_weight,
+    loha_bypass_forward_diff,
 )
