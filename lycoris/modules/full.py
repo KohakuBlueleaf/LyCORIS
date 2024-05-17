d04944791d5458e107f7cf902d05d05732983640
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-05-17 21:51:57 +0800

    Full new base module

--- lycoris/modules/full.py
+++ lycoris/modules/full.py
@@ -85,31 +85,39 @@ class FullModule(LycorisBaseModule):
             diff_bias = state_dict["diff_b"]
             self.bias.data.add_(diff_bias)
 
-    def make_weight(self, scale=1, device=None, original=False):
-        if original:
-            weight = self._org_weight[0].to(device, dtype=self.weight.dtype)
-            if self.org_bias is not None:
-                bias = self.org_bias[0].to(device, dtype=self.bias.dtype)
-            else:
-                bias = None
-            return weight, bias
+    def make_weight(self, scale=1, device=None):
         drop = (
             torch.rand(self.dim, device=device) > self.rank_dropout
             if self.rank_dropout and self.training
             else 1
         )
         if drop != 1 or scale != 1:
-            org_weight = self.org_module[0].weight.to(device, dtype=self.weight.dtype)
-            diff = self.weight.to(device) - org_weight
-            weight = diff * drop * scale + org_weight
-            if self.bias is not None:
-                org_bias = self.org_module[0].bias.to(device, dtype=self.bias.dtype)
-                diff_b = self.bias.to(device) - org_bias
-                bias = diff_b * drop * scale + org_bias
+            diff_w, diff_b = self.get_diff_weight(scale, device=device)
+            weight = self.org_module[0].weight + diff_w * drop
+            bias = self.org_module[0].bias + diff_b * drop
         else:
             weight = self.weight
             bias = self.bias
         return weight, bias
+
+    def get_diff_weight(self, multiplier=1, shape=None, device=None):
+        org_weight = self.org_module[0].weight.to(device, dtype=self.weight.dtype)
+        diff = self.weight.to(device) - org_weight
+        diff_b = None
+        if shape:
+            diff = diff.view(shape)
+        if self.bias is not None:
+            org_bias = self.org_module[0].bias.to(device, dtype=self.bias.dtype)
+            diff_b = self.bias.to(device) - org_bias
+        if device is not None:
+            diff = diff.to(device)
+            if self.bias is not None:
+                diff_b = diff_b.to(device)
+        if multiplier != 1:
+            diff = diff * multiplier
+            if diff_b is not None:
+                diff_b = diff_b * multiplier
+        return diff * multiplier, diff_b
 
     def get_merged_weight(self, multiplier=1, shape=None, device=None):
         weight, bias = self.make_weight(multiplier, device)
@@ -118,6 +126,17 @@ class FullModule(LycorisBaseModule):
             if bias is not None:
                 bias = bias.view(shape[0], 1)
         return weight, bias
+
+    def bypass_forward_diff(self, x, scale=1):
+        diff_weight, diff_bias = self.get_diff_weight(scale)
+        kw_dict = self.kw_dict | {"weight": diff_weight, "bias": diff_bias}
+        return self.op(x, **kw_dict)
+
+    def bypass_forward(self, x, scale=1):
+        org_result = self.org_forward(x)
+        diff_weight, diff_bias = self.get_diff_weight(scale)
+        kw_dict = self.kw_dict | {"weight": diff_weight, "bias": diff_bias}
+        return self.op(org_result, **kw_dict) + org_result
 
     def forward(self, x: torch.Tensor, *args, **kwargs):
         if (
@@ -128,7 +147,28 @@ class FullModule(LycorisBaseModule):
             original = True
         else:
             original = False
+        if original:
+            return self.org_forward(x)
         scale = self.multiplier
-        weight, bias = self.make_weight(scale, x.device, original=original)
+        weight, bias = self.make_weight(scale, x.device)
         kw_dict = self.kw_dict | {"weight": weight, "bias": bias}
         return self.op(x, **kw_dict)
+
+
+
+if __name__ == "__main__":
+    base = nn.Linear(128, 128).cuda()
+    full = FullModule("test", base, 1, 4, 1, weight_decompose=True, factor=8).cuda()
+    print(full)
+    test_input = torch.randn(1, 77, 128).cuda()
+    test_output = full(test_input)
+    torch.sum(test_output).backward()
+    print(test_output.shape)
+
+    base = nn.Conv2d(128, 128, 3, 1, 1)
+    full = FullModule("test", base, 1, 4, 1, weight_decompose=True, use_tucker=True)
+    print(full)
+    test_input = torch.randn(1, 128, 16, 16)
+    test_output = full(test_input)
+    torch.sum(test_output).backward()
+    print(test_output.shape)
