ef1bca1558503323bfe853b0036b5b135c72249f
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-06-25 15:21:58 +0800

    update the import of functional packages

--- lycoris/functional/__init__.py
+++ lycoris/functional/__init__.py
@@ -4,10 +4,17 @@ from .general import (
     power2factorization,
 )
 from .locon import (
+    lora_weight_gen,
     lora_diff_weight,
     lora_bypass_forward_diff,
 )
 from .loha import (
+    loha_weight_gen,
     loha_diff_weight,
     loha_bypass_forward_diff,
+)
+from .lokr import(
+    lokr_weight_gen,
+    lokr_diff_weight,
+    lokr_bypass_forward_diff,
 )
