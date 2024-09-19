2950dc08ae7280c66485bd4043d46c600a993b01
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-09-19 23:20:47 +0800

    3.0.1.dev11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="lycoris_lora",
     packages=find_packages(),
-    version="3.0.1.dev10",
+    version="3.0.1.dev11",
     url="https://github.com/KohakuBlueleaf/LyCORIS",
     description="Lora beYond Conventional methods, Other Rank adaptation Implementations for Stable diffusion",
     author="Shih-Ying Yeh(KohakuBlueLeaf), Yu-Guan Hsieh, Zhidong Gao",
